2cef83bc755783fadbf70fdb134f6fa7d450d92a
Ted Tran <tedtran2019@gmail.com>
2022-08-02 12:39:38 -0700

    Wow, Devise and Rails 7 have an awful lot of issues. Finishing sign up/log in pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 

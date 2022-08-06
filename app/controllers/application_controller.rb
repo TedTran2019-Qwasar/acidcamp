9c89b59595a9caaf04dd778320be0bc4eb625f07
Ted Tran <tedtran2019@gmail.com>
2022-08-06 00:17:38 -0700

    Cleaned up some things related to routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,27 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
-  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   # Resource simply refers to the User model
   def after_sign_in_path_for(resource)
-    if resource.has_role? :super_admin
-      # Send to active admin dashboard
-    else
-      # Page they were previously looking at -> projects page -> root
-      session[:return_to] || projects_url || root_url
+    # if resource.has_role? :super_admin -> admin dashboard
+    # Page they were previously looking at -> projects page -> root
+    if session[:return_to]
+      tmp = session[:return_to]
+      session[:return_to] = nil
+      return tmp
     end
+    projects_url || root_url
   end
 
   def redirect_if_signed_in
     redirect_to projects_url if user_signed_in?
+  end
+
+  # Eh, don't need to bother with this since they're any pages anyway lol.
+  def store_location
+    session[return_to] = request.referer
   end
 
   private
